a5fc9c72418226aa17c911cb8870859f847c2fa5
EduardoXI <eduardogomes3365@gmail.com>
2023-05-09 22:30:31 +0000

    First Commit

---
+++ src/components/Conteudo.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+
+export default function Conteudo() {
+    return (
+        <div className='conteudo'>
+            <div className='center'>
+            <div className='conteudo-single'>
+                <h3>Título da Chamada</h3>
+                <p>Aqui é o meu conteúdo de exemplo para o site da Danki Code no youtube! Espero que vc goste :D</p>
+            </div>
+            <div className='conteudo-single'>
+                <h3>Título da Chamada</h3>
+                <p>Aqui é o meu conteúdo de exemplo para o site da Danki Code no youtube! Espero que vc goste :D</p>
+            </div>
+            <div className='conteudo-single'>
+                <h3>Título da Chamada</h3>
+                <p>Aqui é o meu conteúdo de exemplo para o site da Danki Code no youtube! Espero que vc goste :D</p>
+            </div>
+            <div className='conteudo-single'>
+                <h3>Título da Chamada</h3>
+                <p>Aqui é o meu conteúdo de exemplo para o site da Danki Code no youtube! Espero que vc goste :D</p>
+            </div>
+        </div>
+      </div>
+    )
+}
